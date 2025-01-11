6c77513e723dddca51efa46b306347e65b157ea7
arslan <arslanmohd196@gmail.com>
2025-01-11 22:52:24 +0530

    update

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -4,7 +4,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useEffect } from "react";
 
-export default function SuccessView() {
+const SuccessViewCore = () => {
     const searchParams = useSearchParams()
     const checkoutId = searchParams?.get("checkout_id");
     const txnid = searchParams?.get("txnid");
@@ -22,17 +22,22 @@ export default function SuccessView() {
         }
     }
 
-    return <Suspense>
-        <div className="flex flex-col items-center">
-            <div className="w-64">
-                <DotLottieReact
-                    src="https://lottie.host/da04a8ab-5bce-4c79-8e13-578b2aec8ad6/erX0g5uVEK.lottie"
-                    // loop
-                    autoplay
-                />
-                <h3 className="mt-8 text-3xl text-center font-semibold text-gray-200">Your Payment was successfull!</h3>
-                <h3 className="mt-8 text-base text-center font-semibold text-gray-400">Redirecting you to order list page</h3>
-            </div>
+
+    return <div className="flex flex-col items-center">
+        <div className="w-64">
+            <DotLottieReact
+                src="https://lottie.host/da04a8ab-5bce-4c79-8e13-578b2aec8ad6/erX0g5uVEK.lottie"
+                // loop
+                autoplay
+            />
+            <h3 className="mt-8 text-3xl text-center font-semibold text-gray-200">Your Payment was successfull!</h3>
+            <h3 className="mt-8 text-base text-center font-semibold text-gray-400">Redirecting you to order list page</h3>
         </div>
+    </div>
+}
+
+export default function SuccessView() {
+    return <Suspense>
+        <SuccessViewCore />
     </Suspense>;
 }
